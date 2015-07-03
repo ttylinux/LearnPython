06f6df1285a8cf35f457c2754173fe958ff02ffa
albert <albertxiaoyu@163.com>
2015-07-03 13:36:22 +0800

    *args; **kwargs
    *args，表示参数是一个tuple；
    **kwargs，表示参数是键值对，kwargs中包含的是key。

---
+++ args_kwargs.py
@@ -0,0 +1,32 @@
+
+
+#*args被解释为一个tuple，元组
+def foo(*args):
+    for item in args:
+        print(item)
+
+#**kwargs被解释为一个键值对key-value中的Key
+def bar(**kwargs):
+    for key in kwargs:
+        print(key,kwargs[key])
+	
+
+#解包参数列表,*l
+#unpack argument lists	
+def test(one,two):
+    print(one,two)
+
+list = ['one','two']
+test(*list) #解包参数列表
+print('------------------------')
+
+
+foo(1,2,45)
+print('------------------------')
+foo('a','b','def')
+print('------------------------')
+bar(name='peter',age=25)
+print('------------------------')
+bar(country='China',city='SH')
+print('-----------------------')
+
