887bf62002e3a23fe1d3c6b4c26df9ddce8f4826
LuShuWei <albertxiaoyu@163.com>
2015-07-07 22:34:30 +0800

    测试__init__方法
    调用实例的时候，__init__方法是会执行的。

---
+++ addrBookEntry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class AddrBookEntry(object):
+    'address book entry class'
+    def __init__(self,nm,ph):
+        self.name = nm
+        self.phone = ph
+        print('Created instance for:',self.name)  # 测试init是否被调用了
+
+	
+class EmplAddrBookEntry(AddrBookEntry):
+    'Empoyee Address Book Entry class'	
+    def __init__(self,nm,ph,email):
+        AddrBookEntry.__init__(self,nm,ph)
+        self.email = email
+        print('Created instance EmplAddrBookEntry.')
+	
+def test():
+    one = AddrBookEntry('Peter',120)
+    sub = EmplAddrBookEntry('John',110,'John@gmail.com')
