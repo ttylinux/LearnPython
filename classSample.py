68088057de41aa9bc8b06838156e1b10a241d8e7
albert <albertxiaoyu@163.com>
2015-07-07 15:55:31 +0800

    类的继承和类的多态
    extend;poly

---
+++ classSample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+		
+class Dog(Animal):
+    def run(self):
+        print('Dog is running..')
+		
+	
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+		
+		
+def run_twice(animal):
+    if isinstance(animal,Animal):
+        animal.run()
+    else:
+        print('param type is incoreect,please make sure the param type is Animal')
+    
+    
+	
