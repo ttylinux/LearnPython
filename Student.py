fec9dcc0f66f5549c6ff9b1dcf2b47062b878030
albert <albertxiaoyu@163.com>
2015-07-07 14:10:10 +0800

    python class

---
+++ Student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+		
+
+    def print_score(self):
+        print('%s: %s'%(self.name,self.score))
+		
